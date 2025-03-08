505a22e957b0c605d313ef02878ff3ae90e71a53
Alijonov <alijonov.me@gmail.com>
2025-03-08 13:02:47 +0500

    The security level has been increased.

--- movie/settings.py
+++ movie/settings.py
@@ -98,12 +98,12 @@ DATABASES = {
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': "api.exceptions.exception",
 
-    # 'DEFAULT_RENDERER_CLASSES': [
-    #     'rest_framework.renderers.JSONRenderer',
-    # ],
-    # 'DEFAULT_PARSER_CLASSES': [
-    #     'rest_framework.parsers.JSONParser',
-    # ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
